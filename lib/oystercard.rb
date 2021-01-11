617f19e2995942a98e78fee60622ea55009ebfaf
jshields123 <jackshields@live.co.uk>
2021-01-11 15:30:45 +0000

    enforced max balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
 attr_reader :balance
+MAX_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(added)
+    fail "card full, #{@balance} already in balance" if @balance + added > MAX_BALANCE
     @balance = added
   end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,7 +7,17 @@ describe Oystercard do
 
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
-  it "allows yout to top up balance" do
+  it "allows you to top up balance" do
     expect{ subject.top_up 5 }.to change{ subject.balance}.by 5
   end
+
+  # maximum balance in a constant variable and test will raise error when
+  #balance over $90
+  it 'raises an error when maximum balance is exceeded' do
+    max = Oystercard::MAX_BALANCE
+    subject.top_up(max)
+    expect{ subject.top_up(1) }.to raise_error "card full, #{max} already in balance"
+  end
+
+
 end
