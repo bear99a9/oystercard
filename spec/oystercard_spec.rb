3d45a1b3901f42dc0b33df6f1241676a125d290f
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-13 15:05:54 +0000

    refactored spec test with contexts

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,24 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject { described_class.new }
   let(:topped_up_card) { Oystercard.new(Oystercard::MAX_BALANCE) }
   let(:entry_station) { double :entry_station }
   let(:exit_station) { double :exit_station }
   let(:journey){ {entry_station: entry_station, exit_station: exit_station} }
 
-  it 'has a balance of zero' do
-    expect(subject.balance).to eq(Oystercard::DEFAULT_BALANCE)
+  context 'on initialization the card' do
+    it 'has a balance of zero' do
+      expect(subject.balance).to eq(Oystercard::DEFAULT_BALANCE)
+    end
+
+    it 'is initially not in a journey' do
+      expect(subject).not_to be_in_journey
+    end
+
+    it 'has an empty list of journeys by default' do
+      expect(subject.journeys).to be_empty
+    end
   end
 
   describe '#top_up' do
@@ -18,66 +29,62 @@ describe Oystercard do
       expect{ subject.top_up Oystercard::MIN_BALANCE }.to change{ subject.balance }.by Oystercard::MIN_BALANCE
     end
 
-    it 'raises an error when maximum balance is exceeded' do
-      topped_up_card
-      expect{ topped_up_card.top_up(Oystercard::MIN_BALANCE) }.to raise_error "Cannot top up by £#{Oystercard::MIN_BALANCE}. Balance already at £#{Oystercard::MAX_BALANCE}"
+    context 'when maximum balance is exceeded' do
+      it 'raises an error' do
+        topped_up_card
+        expect{ topped_up_card.top_up(Oystercard::MIN_BALANCE) }.to raise_error "Cannot top up by £#{Oystercard::MIN_BALANCE}. Balance already at £#{Oystercard::MAX_BALANCE}"
+      end
     end
   end
 
-  it 'is initially not in a journey' do
-    expect(subject).not_to be_in_journey
-  end
-
-  context "touch out" do
-    it "can touch out" do
-      topped_up_card.touch_in(entry_station)
-      topped_up_card.touch_out(exit_station)
-      expect(topped_up_card).not_to be_in_journey
+  describe 'journey' do
+    context 'If card has insufficient funds to touch in' do
+      it 'it raises an error' do
+        expect{ subject.touch_in(entry_station) }.to raise_error "Insufficient funds for journey"
+      end
     end
 
-    it 'deducts money from balance' do
-      topped_up_card.touch_in(entry_station)
-      expect { topped_up_card.touch_out(exit_station) }.to change{ topped_up_card.balance}.by(-Oystercard::MIN_FARE)
+    context 'topped up card' do
+      it "can touch in" do
+        topped_up_card.touch_in(entry_station)
+        expect(topped_up_card).to be_in_journey
+      end
+
+      it "touch_in can store entry station" do
+        topped_up_card.touch_in(entry_station)
+        expect(topped_up_card.entry_station).to eq entry_station
+      end
     end
 
-    it 'wipes entry station variable' do
-      topped_up_card.touch_in(entry_station)
-      expect { topped_up_card.touch_out(exit_station) }.to change{ topped_up_card.entry_station}.to(nil)
+    context "touch out" do
+      it "card can touch out" do
+        topped_up_card.touch_in(entry_station)
+        topped_up_card.touch_out(exit_station)
+        expect(topped_up_card).not_to be_in_journey
+      end
+
+      it 'deducts money from balance' do
+        topped_up_card.touch_in(entry_station)
+        expect { topped_up_card.touch_out(exit_station) }.to change{ topped_up_card.balance}.by(-Oystercard::MIN_FARE)
+      end
+
+      it "touch_out method to store exit station" do
+        topped_up_card.touch_in(entry_station)
+        topped_up_card.touch_out(exit_station)
+        expect(topped_up_card.exit_station).to eq exit_station
+      end
+
+      it 'wipes entry station variable' do
+        topped_up_card.touch_in(entry_station)
+        expect { topped_up_card.touch_out(exit_station) }.to change{ topped_up_card.entry_station}.to(nil)
+      end
+
+      it 'stores the journey' do
+        topped_up_card.touch_in(entry_station)
+        topped_up_card.touch_out(exit_station)
+        expect(topped_up_card.journeys).to include(journey)
+      end
     end
-
-    it "touch_out method to store exit station" do
-      topped_up_card.touch_in(entry_station)
-      topped_up_card.touch_out(exit_station)
-      expect(topped_up_card.exit_station).to eq exit_station
-    end
-
-  end
-
-  it "can touch in" do
-    topped_up_card.touch_in(entry_station)
-    expect(topped_up_card).to be_in_journey
-  end
-
-  it "touch_in method to store entry station" do
-    topped_up_card.touch_in(entry_station)
-    expect(topped_up_card.entry_station).to eq entry_station
-  end
-
-  context 'No credit on card' do
-    it 'will not touch in if below minimum balance' do
-      expect{ subject.touch_in(entry_station) }.to raise_error "Insufficient funds for journey"
-    end
-  end
-
-  it 'has an empty list of journeys by default' do
-    card = Oystercard.new
-    expect(card.journeys).to be_empty
-  end
-
-  it 'stores the journey' do
-    topped_up_card.touch_in(entry_station)
-    topped_up_card.touch_out(exit_station)
-    expect(topped_up_card.journeys).to include(journey)
   end
 
 end
